92144e266d2039886b6fe11dcf83f6010826be7b
Ryan Hefferon <ryheff09@gmail.com>
2026-03-16 18:48:43 -0400

    finish turret documentation

--- src/main/java/frc/robot/subsystems/shooter/turret/Turret.java
+++ src/main/java/frc/robot/subsystems/shooter/turret/Turret.java
@@ -23,20 +23,32 @@ import java.util.function.Supplier;
 import org.littletonrobotics.junction.Logger;
 
 public class Turret extends FullSubsystem {
+  //creating objects and initializing variables
+  //Shooter side object, which applies not only to the turret which consists of 1 motor but the flywheel and hood as well, as all of these components work together to create one shooter)
   private final ShooterSide side;
 
+  //standard turret io object
   private final TurretIO io;
+  //standard auto logged class which is automatically generated due to the use of @Autolog in the io interface
   private final TurretIOInputsAutoLogged inputs = new TurretIOInputsAutoLogged();
+  //An instance of the outputs class created in the io interface
   private final TurretIOOutputs outputs = new TurretIOOutputs();
 
+  //sets the target angle of the turret equal to the zeroed angle
   private Rotation2d targetAngle = Rotation2d.kZero;
 
+  //the default value for at goal is false, as the turret has not moved yet
   private boolean atGoal = false;
+  //A debouncer object which controls the boolean output of whether or not the turret is at its goal
+  //When the turret reaches its target and continues past it, it must remain as false for 0.1 seconds before the signal is actually read as false
+  //This ensures that the turret actually stops at its target and doesn't skip over it
   private Debouncer atGoalDebouncer = new Debouncer(0.1, DebounceType.kFalling);
 
+  //the default value for iszeroed is false, as the turret has not been calibrated yet
   private boolean isZeroed = false;
 
   /** Creates a new Turret. */
+  //standard constructor
   public Turret(ShooterSide side, TurretIO io) {
     this.side = side;
     this.io = io;
@@ -44,34 +56,48 @@ public class Turret extends FullSubsystem {
 
   @Override
   public void periodic() {
+    //standard data logging
     io.updateInputs(inputs);
     Logger.processInputs(("Turret/" + side.getName()), inputs);
 
+    //if a limit switch is triggered, the turret is zeroed
     if (inputs.limitTriggered) {
       isZeroed = true;
     }
 
+    //if the shooter side is left, the left turret angle is set to the target angle input, and vice versa for the right
     if (side == ShooterSide.LEFT) {
       RobotVisualizer.getInstance().setLeftTurretAngle(Rotation2d.fromRadians(inputs.positionRad));
     } else if (side == ShooterSide.RIGHT) {
       RobotVisualizer.getInstance().setRightTurretAngle(Rotation2d.fromRadians(inputs.positionRad));
     }
 
+    //The logger also stores the values of the turret side and what its target is (particularly useful in debugging)
     Logger.recordOutput(("Turret/" + side.getName() + "/TargetAngle"), targetAngle);
   }
 
+  //The periodic after scheduler method is called periodically after the period method, particularly to apply outputs
+  //You could just put everything in this method at the end of the periodic method, but I guess this is just for organization
   @Override
   public void periodicAfterScheduler() {
+    //calls the default apply outputs method, which sets the motor to a certain speed to reach a target using closed loop controllers in both the hardware and simulation layers
     io.applyOutputs(outputs);
   }
 
+  //A command to track the motor's progress to reaching the target
+  //takes a supplier which returns a translation 2d value, which represents points or vectors on a coordinate plane
   public Command trackTarget(Supplier<Translation2d> targetSupplier) {
 
+    //the command is a run command, meaning it executes the actions periodically until interrupted
     return Commands.run(
         () -> {
+          //sets a translation2d target value equal to the target supplier (remember that suppliers can be changed, unlike variables, because they are functions)
           Translation2d target = targetSupplier.get();
+          //sets a pose2d object, which is a combination of translation2d and rotation2d, to get the robot's approximate position and direction on the field
           Pose2d robotPose = RobotState.getInstance().getEstimatedPose();
 
+          //takes a transform 3d object (point in 3d space, or translation 3d, + rotation2d) defined in constants and gets the translation2d from it
+          //if the shooter side is left, it calls the left turret transform 3d object, and vice versa
           Translation2d turretOffset =
               (this.side == ShooterSide.LEFT
                   ? TurretConstants.kRobotToLeftTurret.getTranslation().toTranslation2d()
@@ -90,6 +116,7 @@ public class Turret extends FullSubsystem {
           // Angle turret should point (robot-relative)
           Rotation2d targetAngle = new Rotation2d(Math.atan2(deltaRobot.getY(), deltaRobot.getX()));
 
+          //after getting the target angle of the turret based on the robots position and rotation on the field, this command references the method below to actually set the motor to the target
           this.targetAngle = targetAngle;
 
           setPosition(targetAngle);
@@ -97,6 +124,7 @@ public class Turret extends FullSubsystem {
         this);
   }
 
+  //automatically zeroes the turret by running it at a certain speed until it hits the limit switch, using the is zeroed method defined above to determine this
   public Command zero() {
     return Commands.startEnd(() -> setOpenLoop(0.2), () -> stop()).until(this::isZeroed);
   }
@@ -109,10 +137,12 @@ public class Turret extends FullSubsystem {
    * @param position A {@link Rotation2d} object representing the target position of the turret.
    */
   public void setPosition(Rotation2d position) {
+    //if the turret is not zeroed the method stops
     if (!isZeroed) return; // safety
 
     targetAngle = position;
 
+    //clamps the position between the turret's movement limits and converts it to radians
     position =
         Rotation2d.fromRadians(
             MathUtil.clamp(
@@ -120,24 +150,37 @@ public class Turret extends FullSubsystem {
                 TurretConstants.kMinTurretAngleRad,
                 TurretConstants.kMaxTurretAngleRad));
 
+    //again sets the turret mode to closed loop, and updates the value of the closed loop target based on the position parameter
     outputs.mode = TurretIOOutputMode.CLOSED_LOOP;
     outputs.closedLoopTarget = position;
 
+    //sets a variable at goal equal to the debounced value of whether or not the absolute value of the goal position minus the actual position (an input using the encoder) is less than the tolerance, or the acceptable level of error, defined in constants
+    //Again, the debouncer prevents the motor from moving too fast and going past the target without the code noticing
     atGoal =
         atGoalDebouncer.calculate(
             Math.abs(position.getRadians() - inputs.positionRad) < TurretConstants.kAngleTolerance);
   }
 
+  //a safety method which clamps the open loop output defined in the outputs class between the standard -1.0 and 1.0 used to set the speed of a motor
   public void setOpenLoop(double output) {
     outputs.mode = TurretIOOutputMode.OPEN_LOOP;
     outputs.openLoopOutput = MathUtil.clamp(output, -1.0, 1.0);
   }
 
+  //another safety method which sets the open loop output defined in the outputs class to 0
   public void stop() {
     outputs.mode = TurretIOOutputMode.OPEN_LOOP;
     outputs.openLoopOutput = 0.0;
   }
 
+  //all the methods below return information about the state of the turret: 
+  //the position (returns corresponding input)
+  //the velocity (returns corresponding input)
+  //whether or not the motor has reached its goal (defined in the command above)
+  //whether or not the motor is zeroed (defined in the command above)
+  //what the side of the shooter is (defined in a different shooter file)
+  
+  //note that all the commands do is set the change the values of the outputs in the outputs class; the method apply outputs does all the work of actually moving the motors
   public double getPosition() {
     return inputs.positionRad;
   }
--- src/main/java/frc/robot/subsystems/shooter/turret/TurretIOSim.java
+++ src/main/java/frc/robot/subsystems/shooter/turret/TurretIOSim.java
@@ -14,14 +14,14 @@ public class TurretIOSim implements TurretIO {
   //standard simulated gearbox object
   private final DCMotorSim sim;
 
-  //creates a pid controller
+  //creates a pid controller with gains (these need to be tuned; see PID documentation for more details)
   private PIDController pid = new PIDController(2, 0, 0.3, Constants.kLoopPeriodSeconds);
 
   //standard applied volts variable to be implemented later
   private double appliedVolts = 0.0;
 
   public TurretIOSim() {
-    //resets the pid controller by clearing any previous errors and the integral term
+    //resets the pid controller by clearing any previous errors that it was storing and the integral term
     pid.reset();
     //enables continuous movement for the pid (past 360 degrees and back to 0)
     //sets the minimum and maximum to radian values
@@ -29,19 +29,25 @@ public class TurretIOSim implements TurretIO {
     //ex: if the target is 1 degree and the turret is at 350, the turret will only move 11 degrees clockwise rather than 349 degrees counterclockwise
     //question: why set the actual turret not to continuous movement but the pid to continuous movement?
     pid.enableContinuousInput(-Math.PI, Math.PI);
+    //default sim object
     sim =
         new DCMotorSim(
             LinearSystemId.createDCMotorSystem(gearbox, 0.025, TurretConstants.kGearRatio),
             gearbox);
   }
 
+  //standard default update input method
   @Override
   public void updateInputs(TurretIOInputs inputs) {
+    //Keeps the value of the voltage in an acceptable range (-12 to 12)
     double volts = MathUtil.clamp(appliedVolts, -12.0, 12.0);
 
+    //Sets the voltage of the motor to the value of volts clamped above
     sim.setInputVoltage(volts);
+    //updates the states of the dc motor simulation object every 0.02 seconds to be logged as data points
     sim.update(0.02);
 
+    //updates all the input values using the simulation object
     inputs.connected = true;
     inputs.positionRad = sim.getAngularPositionRad();
     inputs.velocityRadPerSec = sim.getAngularVelocityRadPerSec();
@@ -49,13 +55,18 @@ public class TurretIOSim implements TurretIO {
     inputs.currentDrawAmps = sim.getCurrentDrawAmps();
   }
 
+  //default apply outputs method
   @Override
   public void applyOutputs(TurretIOOutputs outputs) {
+    //again, the switch, case, case keywords are a simpler version of conditional statements
     switch (outputs.mode) {
+      //in the case that the mode is closed loop (which it is as of now), the pid setpoint is set to the specified target in radians
+      //calculates the voltage which needs to be applied to the simulated gearbox object by getting the current position of the turret in radians
       case CLOSED_LOOP -> {
         pid.setSetpoint(outputs.closedLoopTarget.getRadians());
         appliedVolts = pid.calculate(sim.getAngularPositionRad());
       }
+      //in the case that the mode is changed to open loop, the applied voltage is set to 12 times the speed of the motor (ex 0.5 speed is 50%, which is equivalent to 6 out of 12 volts from the batter)
       case OPEN_LOOP -> {
         appliedVolts = 12.0 * outputs.openLoopOutput;
       }
