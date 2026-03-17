ffcc7b8c7d55aeb5aac95331a8411f0d0b4dcf39
Ryan Hefferon <ryheff09@gmail.com>
2026-03-17 17:23:59 -0400

    finish hood documentation

--- src/main/java/frc/robot/subsystems/shooter/hood/Hood.java
+++ src/main/java/frc/robot/subsystems/shooter/hood/Hood.java
@@ -22,7 +22,13 @@ public class Hood extends SubsystemBase {
   //standard autologged class
   private final HoodIOInputsAutoLogged inputs = new HoodIOInputsAutoLogged();
 
+  //the default value for whether or not the turret is at its goal is false, as it hasn't moved yet
   private boolean atGoal = false;
+
+  //A debouncer object which controls the boolean output of whether or not the turret is at its goal
+  //When the turret reaches its target and continues past it, it must remain as false for 0.2 seconds before the signal is actually read as false
+  //This ensures that the turret actually stops at its target and doesn't skip over it
+  //This time value is higher than that of the turret; is this because the hood moves faster?
   private Debouncer atGoalDebouncer = new Debouncer(0.2, DebounceType.kFalling);
 
   /** Creates a new Hood. */
@@ -33,9 +39,11 @@ public class Hood extends SubsystemBase {
 
   @Override
   public void periodic() {
+    //default update inputs and data storage
     io.updateInputs(inputs);
     Logger.processInputs(("Hood/" + side.getName()), inputs);
 
+    //sets the angle of the left or right turret to the position in radians input depending again on the shooter side
     if (side == ShooterSide.LEFT) {
       RobotVisualizer.getInstance().setLeftHoodAngle(inputs.positionRad);
     } else if (side == ShooterSide.RIGHT) {
@@ -49,15 +57,25 @@ public class Hood extends SubsystemBase {
    * @param angle The target angle (in radians).
    */
   public void setAngle(double angle) {
+    //sets an at goal variable equal to the debounced value of whether or not the absolute value of the target minus the actual position of the hood (referenced using the input), which is how far the hood is from its target
+
     atGoal =
         atGoalDebouncer.calculate(
             Math.abs(angle - inputs.positionRad) < HoodConstants.kAngleTolerance);
+    //sets the angle of the hood using the method defined in the io interface, which happens to be named exactly the same as the current method
     io.setAngle(angle);
   }
 
+  //method which sets the speed of the motor, again referencing a default method defined in the io interface
   public void setOpenLoop(double output) {
     io.setOpenLoop(output);
   }
+
+  //The methods below return information about the state of the hood:
+  //The position (referencing the relevant input)
+  //the velocity (referencing the relevant input)
+  //the goal (referencing the at goal variable defined above in the set angle method)
+  //the side of the shooter (ultimately defined in a different shooter file)
 
   public double getPosition() {
     return inputs.positionRad;
